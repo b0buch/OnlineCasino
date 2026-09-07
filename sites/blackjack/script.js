f972a8da08c79502e23688716f915ac2dcd7f9e3
Zeeeeeeno <zeno.fuchs@noseryoung.ch>
2026-09-07 15:27:31 +0200

    oh manno 15:27. keine ahnugn wa sicvh gemacht habe

--- sites/blackjack/script.js
+++ sites/blackjack/script.js
@@ -185,3 +185,36 @@ function endGame(msg, newBalance) {
 
   updateUI();
 }
+
+
+
+
+
+// sprache
+
+const translations = {
+    de: {
+        title: "Online Casino",
+        money: "Geld: xxx$",
+        blackjack: "Blackjack",
+        info: "Info"
+    },
+
+    en: {
+        title: "Online Casino",
+        money: "Money: xxx$",
+        blackjack: "Blackjack",
+        info: "Info"
+    }
+};
+
+function setLanguage(language) {
+    document.getElementById("title").textContent =
+        translations[language].title;
+
+    document.getElementById("money").textContent =
+        translations[language].money;
+
+    document.getElementById("blackjack").textContent =
+        translations[language].blackjack;
+}
